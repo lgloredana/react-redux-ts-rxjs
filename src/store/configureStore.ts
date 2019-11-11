de821c3ead39b3b5a657d51e4a954ce13c0e56e1
Loredana Lungu <loredana.lungu@tradeshift.com>
2019-11-11 16:32:42 +0200

    add extension for dev tool

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,14 +1,26 @@
 import {composeWithDevTools} from "redux-devtools-extension";
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunk from "redux-thunk";
+import { routerReducer } from "react-router-redux";
 import checker from "./middlewares/checker";
 import logger from "./middlewares/logger";
 import {todoReducer} from "./boiler/todos/reducers";
 import {goalReducer} from "./boiler/goals/reducers";
 
-const rootReducer = combineReducers({todos: todoReducer, goals: goalReducer});
-const rootMiddleware = applyMiddleware(thunk, checker, logger);
+// rehydrate state on app start
+const initialState = {};
+
+const rootReducer = combineReducers({
+    router: routerReducer,
+    todos: todoReducer,
+    goals: goalReducer
+});
+const composeEnhancers = composeWithDevTools({
+     name: 'POC RXJS-React-Redux-TypeScript ====Store====',
+});
+// const rootMiddleware = composeWithDevTools(applyMiddleware(thunk, checker, logger));
+const rootMiddleware = composeEnhancers(applyMiddleware(thunk, checker, logger));
 
 export default function configureStore() {
-    return createStore(rootReducer, composeWithDevTools(rootMiddleware));
+    return createStore(rootReducer, initialState,rootMiddleware);
 }
